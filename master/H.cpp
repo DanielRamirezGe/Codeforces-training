88d45e6cd07d088cc3cf32d6da3bdbd3a2ea38cf
DanielRamirezGe <42500282+DanielRamirezGe@users.noreply.github.com>
2021-01-12 20:44:46 -0600

    add problems
    add problems

--- master/H.cpp
+++ master/H.cpp
@@ -1,52 +1,47 @@
 #include <bits/stdc++.h>
-#define maxn 1000005
-
 using namespace std;
-
-int arr[ maxn ];
-
-int  l_b( int i, int j, int value )
-{
-	int m;
-
-	if( value <= 0 ) return 0;
-	if( j == 1 ) return ( arr[ 1 ] + arr[ 0 ] <= value ? 1 : 0 );
-
-	while( j-i > 1 )
-	{
-		m = i + ((j-i)>>1);
-		if ( arr[ m ] > value )
-			j = m;
-		else
-			i = m;
-	}
-	return (arr[ i ] == value ? i : i+1);
-}
-
-long long f( int n, int q )
-{
-	long long ans = 0;
-	for (int i = 1; i < n && arr[ i ]; ++i)
-		ans += l_b( 0, i, q-arr[i] );
-
-	return ans;
-}
-
 int main()
 {
-	ios_base::sync_with_stdio( 0 );
-	cin.tie( 0 );
-	
-	int n, q, a;
-	cin >> n >> q;
-	for (int i = 0; i < n; ++i)
-		cin >> arr[ i ];
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
-	sort( arr, arr+n );
-	for (int i = 0; i < q; ++i)
+	int x,num,querys;
+	cin>>num>>querys;
+	vector <int> vec;
+	for(int i=0; i<num; i++)
 	{
-		cin >> a;
-		cout << f( n, a ) << '\n';
+		cin>>x;
+		vec.push_back(x);
 	}
-	return 0;
+	sort(vec.begin(),vec.end());
+	for(int i=0; i<querys; i++)
+	{
+		cin>>x;
+		long long suma=0;
+		
+		for(int j=0; j<vec.size(); j++)
+		{
+			int busca= x-vec[j];
+
+			auto it=lower_bound(vec.begin(),vec.end(), busca);
+			
+			if(it==vec.end())
+				it--;
+			else
+			{
+				if(*it!=busca)
+					it--;
+			}
+			int mas=it-vec.begin();
+			//cout<<"  "<<mas<<endl;
+			mas-=j;
+			//cout<<"   "<<mas<<endl;
+			if(mas>0)
+			{
+				suma+=mas;
+			}
+		}
+		//cout<<i<<endl;
+		cout<<suma<<endl;
+	}
 }
